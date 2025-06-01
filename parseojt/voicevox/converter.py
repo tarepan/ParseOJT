30510d41ad06e27240d4c3dc41ed02d817554315
tarepan <tarepan5884@gmail.com>
2025-06-01 18:07:57 +0000

    add voicevox converter basics

--- parseojt/voicevox/converter.py
+++ parseojt/voicevox/converter.py
@@ -1,10 +1,60 @@
 """Utterance-To-VOICEVOX converter."""
 
-from parseojt.domain import Utterance
-from parseojt.voicevox.domain import AccentPhrase
+from parseojt.domain import Utterance, Word
+from parseojt.voicevox.domain import AccentPhrase, Mora
 
 
-def convert_utterance_to_accent_phrases(utterance: Utterance) -> list[AccentPhrase]:
-    """VV parser."""
-    _ = utterance
-    return [AccentPhrase(["mora1"])]
+def _gen_pau_mora() -> Mora:
+    """Generate VOICEVOX pause mora."""
+    # NOTE: ref: https://github.com/VOICEVOX/voicevox_engine/blob/c95bb9e387043e7f7a2eb4fd3e46692fea28716a/voicevox_engine/tts_pipeline/text_analyzer.py#L383-L391
+    return Mora(
+        text="、",
+        consonant=None,
+        consonant_length=None,
+        vowel="pau",
+        vowel_length=0,
+        pitch=0,
+    )
+
+
+def _convert_words_to_voicevox_moras(words: list[Word]) -> list[Mora]:
+    """Convert words into VOICEVOX moras."""
+    vv_moras: list[Mora] = []
+    for word in words:
+        for mora in word.moras:
+            phonemes = mora.phonemes
+            consonant = None if len(phonemes) == 1 else phonemes[0].symbol
+            consonant_length = None if len(phonemes) == 1 else 0
+            vowel = phonemes[0].symbol if len(phonemes) == 1 else phonemes[1].symbol
+            pron = mora.pronunciation
+            if pron[-1] == "’":
+                mora_text = pron[:-1]
+            elif pron == "ー":
+                mora_text = "a" # TODO: fix here
+            else:
+                mora_text = pron
+            vv_moras.append(
+                Mora(
+                    text=mora_text,
+                    consonant=consonant,
+                    consonant_length=consonant_length,
+                    vowel=vowel,
+                )
+            )
+    return vv_moras
+
+
+def convert_utterance_to_voicevox_accent_phrases(utterance: Utterance) -> list[AccentPhrase]:
+    """Convert utterance into VOICEVOX accent phrases."""
+    vv_aps: list[AccentPhrase] = []
+    for bc in utterance:
+        for ap in bc.accent_phrases:
+            is_tail_ap = ap == bc.accent_phrases[-1]
+            vv_aps.append(
+                AccentPhrase(
+                _convert_words_to_voicevox_moras(ap.words),
+                accent=ap.accent,
+                pause_mora= _gen_pau_mora() if is_tail_ap else None,
+                is_interrogative=ap.interrogative)
+            )
+    return vv_aps
--- parseojt/voicevox/domain.py
+++ parseojt/voicevox/domain.py
@@ -4,7 +4,22 @@ from dataclasses import dataclass
 
 
 @dataclass
-class AccentPhrase:
-    """VOICEVOX AccentPharase."""
+class Mora:
+    """VOICEVOX モーラ。"""
 
-    moras: list[str]
+    text: str
+    consonant: str | None
+    consonant_length: float | None
+    vowel: str
+    vowel_length: float = 0
+    pitch: float = 0
+
+
+@dataclass
+class AccentPhrase:
+    """VOICEVOX アクセント句。"""
+
+    moras: list[Mora]
+    accent: int
+    pause_mora: Mora | None
+    is_interrogative: bool
